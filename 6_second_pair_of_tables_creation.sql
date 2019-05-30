da2fa9448f2b361dc62be0e34e9b7eb726c23b3b
Viktoriia Pavliuk <pavliuk.viktoriia@gmail.com>
2019-05-30 23:13:41 +0300

    added data to tables

--- 6_second_pair_of_tables_creation.sql
+++ 6_second_pair_of_tables_creation.sql
@@ -1,6 +1,4 @@
-
 use VP_module_2
-
 go
 drop table if exists User_Info
 go
@@ -13,7 +11,7 @@ Birth_Date date,
 Phone_number varchar(30),
 Email_Address varchar(100),
 CardType varchar(50),
-Card_Number varchar(20),
+Card_Number varchar(20)NOT NULL UNIQUE,
 Card_Expire_date date,
 CVV int check (CVV between 0 and 9999),
 Secret_Word varchar(20),
@@ -33,3 +31,9 @@ Transaction_Message varchar(150),
 Type_of_Operation varchar (100),
 Date_of_Operation datetime  DEFAULT GETDATE());
 
+INSERT INTO User_Info VALUES
+(0,'Lora','Smith','Jane','1998-01-01','+38(068)-023-32-30','lora.jane.smith@gmail.com"', 'MasterCard','3978-8888-9696-3232','2020-09-01',223,'kitty',2223.03),
+(1,'Emily','Smith','Lauren','1998-01-01','+38(098)-153-25-39','emily.smith@gmail.com"', 'MasterCard','2978-3455-9191-1130','2020-03-01',512,'puppy',33.7);
+
+INSERT INTO Operation_History VALUES
+(0,'Ukrsibbank',0,1,245.7,5,5,'Happy Birthday!','incerted',getdate());
