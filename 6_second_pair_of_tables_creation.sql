461f19693d65ffd79200959e697d3a9a6983f171
Viktoriia Pavliuk <pavliuk.viktoriia@gmail.com>
2019-05-30 21:06:31 +0300

    added more constraints

--- 6_second_pair_of_tables_creation.sql
+++ 6_second_pair_of_tables_creation.sql
@@ -5,7 +5,7 @@ go
 drop table if exists User_Info
 go
 CREATE TABLE User_Info(
-id int IDENTITY PRIMARY KEY,
+id int  NOT NULL  PRIMARY KEY,
 First_Name varchar(20),
 Last_Name varchar(20),
 Middle_Name varchar(20),
@@ -15,22 +15,21 @@ Email_Address varchar(100),
 CardType varchar(50),
 Card_Number varchar(20),
 Card_Expire_date date,
-CVV int,
+CVV int check (CVV between 0 and 9999),
 Secret_Word varchar(20),
 CurrentBalance decimal);
 
 drop table if exists Opertion_History
 go
 CREATE TABLE Operation_History(
-Operation_id int IDENTITY PRIMARY KEY,
+Operation_id int NOT NULL PRIMARY KEY,
 Bank varchar(100),
-SenderID int FOREIGN KEY REFERENCES User_Info(id),
-ReceiverID int FOREIGN KEY REFERENCES User_Info(id),
+SenderID int  FOREIGN KEY REFERENCES User_Info(id),
+ReceiverID int  FOREIGN KEY REFERENCES User_Info(id),
 Amount decimal, 
 Duration_in_Seconds int,
 Comission int,
 Transaction_Message varchar(150),
 Type_of_Operation varchar (100),
 Date_of_Operation datetime NOT NULL DEFAULT GETDATE());
-
 
